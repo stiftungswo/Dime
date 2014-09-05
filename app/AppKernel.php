c0475766e93f1f625261f8972296cf81074734ab
toasterson <toasterson@gmail.com>
2014-09-05 11:34:12 +0200

    Moved Log and Cache Directorys in dev and test to /tmp for better development with vagrant or docker.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,13 +1,14 @@
 <?php
-
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
+
     public function registerBundles()
     {
         $bundles = array(
+            
             // symfony-standard edition
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\SecurityBundle\SecurityBundle(),
@@ -17,6 +18,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            
             // added packages
             new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
@@ -27,20 +29,47 @@ class AppKernel extends Kernel
             new Phpugl\TwitterBootstrapBundle\PhpuglTwitterBootstrapBundle(),
             new Dime\TimetrackerBundle\DimeTimetrackerBundle(),
             new Dime\TimetrackerFrontendBundle\DimeTimetrackerFrontendBundle(),
-            new Dime\ReportBundle\DimeReportBundle(),
+            new Dime\ReportBundle\DimeReportBundle()
         );
-
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        
+        if (in_array($this->getEnvironment(), array(
+            'dev',
+            'test'
+        ))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
-
+        
         return $bundles;
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
+    }
+
+    public function getCacheDir()
+    {
+        if (in_array($this->getEnvironment(), array(
+            'dev',
+            'test'
+        ))) {
+            return '/tmp/app/cache/' . $this->environment;
+        } else {
+            return $this->rootDir . '/cache/' . $this->environment;
+        }
+    }
+
+    public function getLogDir()
+    {
+        if (in_array($this->getEnvironment(), array(
+            'dev',
+            'test'
+        ))) {
+            return '/tmp/app/log/' . $this->environment;
+        } else {
+            return $this->rootDir . '/log/' . $this->environment;
+        }
     }
 }
