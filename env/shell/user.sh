9f26e3f3e9eed14dc98e40f8d335859e4626ae79
ursfrick <urs.frick@gmail.com>
2014-10-09 11:25:16 +0200

    updated user.sh for installation of assets

--- env/shell/user.sh
+++ env/shell/user.sh
@@ -8,3 +8,4 @@ php $COMPOSERDIR/composer.phar install
 php app/console doctrine:schema:create
 php app/console assetic:dump
 php app/console doctrine:fixtures:load -n
+php app/console asset:install --symlink
