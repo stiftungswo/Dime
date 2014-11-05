3dd2a5879ca57d11fbd5ca06c582db2f10614b10
ursfrick <urs.frick@gmail.com>
2014-11-05 10:17:23 +0100

    Replacing of OnDemandGrid for offer positions by OfferPositionWidget

--- Resources/public/js/dime/widget/offer/OfferPositionGrid.js
+++ Resources/public/js/dime/widget/offer/OfferPositionGrid.js
@@ -4,23 +4,54 @@
 define(
     [
         'dojo/_base/declare',
-        'dgrid/OnDemandGrid',
-        'dgrid/extensions/DijitRegistry',
-        'dgrid/Selection',
+        'dojo/store/Memory',
+        'dojo/store/Observable',
+        'dgrid/CellSelection',
         'dgrid/Keyboard',
-        'dijit/form/TextBox',
+        'dgrid/OnDemandGrid',
+        'dgrid/editor',
+        "dijit/InlineEditBox",
+        'dijit/form/FilteringSelect'
     ],
-    function(declare, OnDemandGrid, DijitRegistry, Selection, Keyboard, TextBox) {
-        return declare('dime.widget.offer.OfferPositionGrid', [OnDemandGrid, DijitRegistry, Selection, Keyboard], {
-            store: window.storeManager.get('offerpositions', false, true),
+    function(declare, Memory, Observable, CellSelection, Keyboard, OnDemandGrid, editor, InlineEditBox, FilteringSelect) {
+        return declare('dime.widget.offer.OfferPositionGrid', [OnDemandGrid, CellSelection, Keyboard], {
+            //jsonrest stores
+            //store: window.storeManager.get('offerpositions', false, true),
+            //servicesStore: window.storeManager.get('services', true),
+
+            //in memory stores
+            servicesStore : new Memory({
+                idProperty: "id",
+                data: [
+                    { id: 1, name: "Büro Zivi" },
+                    { id: 2, name: "Feld Zivi" },
+                    { id: 3, name: "Sozialmitarbeiter" },
+                    { id: 4, name: "Fachperson 1" },
+                    { id: 10, name: "Fachperson 2" }
+                ]
+            }),
+
+            store : new Observable(new Memory({
+                data: [
+                    { id: "1", order: 10, service:1, rate: 50, vat:8, discountable:false },
+                    { id: "2", order: 20, service:2, rate: 55, vat:8, discountable:false },
+                    { id: "3", order: 30, service:2, rate: 80, vat:8, discountable:false },
+                    { id: "4", order: 40, service:3, rate: 120, vat:8, discountable:false },
+                ]
+            })),
+
             columns: [
                 {label: 'ID', field: 'id', autoSave: true, sortable: true},
                 {label: 'Reihenfolge', field: 'order', autoSave: true, sortable: true},
-                {label: 'Service', field: 'service', autoSave: true, sortable: true},
-                {label: 'Tariv', field: 'rate', autoSave: true, sortable: true},
+                editor({label: 'Service', field: 'service', autoSave: true, sortable: false, editor:FilteringSelect, editorArgs: {store: this.servicesStore}}),
+                //{label: 'Service', field: 'service', autoSave: true, sortable: false},
+                {label: 'Tarif', field: 'rate', autoSave: true, sortable: true },
                 {label: 'Mwst', field: 'vat', autoSave: true, sortable: true},
                 {label: 'Rabattierbar', field: 'discountable', autoSave: true}
             ]
+
+
+
         })
     }
 );
