eba9939804ffc9e936ee4477c5a931217e880697
toasterson <toasterson@gmail.com>
2014-11-14 14:49:21 +0100

    Added: RateGroup to GUI. Fixed: Delete in Grid not Updating Properly. Fixed: Wrong ID in view.html.twig for tags

---
+++ Resources/public/js/dime/widget/rategroup/RateGroupGrid.js
@@ -0,0 +1,24 @@
+/**
+ * Created by toast on 9/30/14.
+ */
+define(
+    [
+        'dojo/_base/declare',
+        'dgrid/OnDemandGrid',
+        'dgrid/extensions/DijitRegistry',
+        'dgrid/Selection',
+        'dgrid/Keyboard',
+        'dgrid/editor',
+        'dijit/form/TextBox'
+    ],
+    function(declare, OnDemandGrid, DijitRegistry, Selection, Keyboard, editor) {
+        return declare('dime.widget.rategroup.RateGroupGrid', [OnDemandGrid, DijitRegistry, Selection, Keyboard], {
+            store: window.storeManager.get('rategroups',false, true),
+            sort: 'name',
+            columns: [
+                editor({label: 'Name', field: 'name', autoSave: true}, 'dijit/form/TextBox', 'dblclick'),
+                editor({label: 'Beschreibung', field: 'description', autoSave: true}, 'dijit/form/TextBox', 'dblclick')
+            ]
+        })
+    }
+);
---
+++ Resources/public/js/dime/widget/rategroup/RateGroupsWidget.js
@@ -0,0 +1,41 @@
+/**
+ * Created by Till Wegmüller on 11/13/14.
+ */
+define([
+    'dijit/_WidgetsInTemplateMixin',
+    'dijit/_TemplatedMixin',
+    'dime/widget/_Base',
+    'dojo/_base/declare',
+    'dojo/text!dime/widget/rategroup/templates/RateGroupsWidget.html',
+    'dojo/when',
+    'dime/widget/rategroup/RateGroupGrid',
+    'dijit/form/Button'
+], function ( WidgetsInTemplateMixin, TemplatedMixin,  _Base, declare, template, when) {
+    return declare("dime.widget.rategroup.RateGroupsWidget", [_Base, TemplatedMixin, WidgetsInTemplateMixin], {
+
+        templateString: template,
+        baseClass: "rategroupsWidget",
+        store: window.storeManager.get('rategroups',false, true),
+
+
+        _setupChildren: function(){
+            this.GridNode.set('parentWidget', this);
+            this.deleteNode.set('parentWidget', this);
+            this.addNode.set('parentWidget', this);
+        },
+
+        _addcallbacks: function(){
+            this.deleteNode.on('click', function(){
+                //this in the button
+                for(var id in this.parentWidget.GridNode.selection){
+                    this.parentWidget.store.remove(id);
+                }
+            });
+            this.addNode.on('click', function(){
+                //this in the button
+                this.parentWidget.store.add({ name:'NewRateGroup'});
+            });
+        }
+    });
+});
+
