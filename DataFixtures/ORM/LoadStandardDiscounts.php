f52bbdbe3baa9bc77260e50111e0d560221229be
ursfrick <urs.frick@gmail.com>
2014-11-17 14:46:47 +0100

    StandardDiscount

---
+++ DataFixtures/ORM/LoadStandardDiscounts.php
@@ -0,0 +1,50 @@
+<?php
+namespace Dime\TimetrackerBundle\DataFixtures\ORM;
+
+use Dime\TimetrackerBundle\Entity\StandardDiscount;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadStandardDiscounts extends AbstractFixture implements OrderedFixtureInterface
+{
+	/**
+	 * Load data fixtures with the passed EntityManager
+	 *
+	 * @param Doctrine\Common\Persistence\ObjectManager $manager
+	 */
+	public function load(ObjectManager $manager)
+	{	
+
+		$discount1 = new StandardDiscount();
+        $discount1->setName('Skonto 2%');
+        $discount1->setMinus(true);
+        $discount1->setPercentage(true);
+        $discount1->setValue(0.2);
+        $discount1->setUser($manager->merge($this->getReference('default-user')));
+		$this->addReference('skonto_standarddiscount', $discount1);
+		$manager->persist($discount1);
+
+        $discount2 = new StandardDiscount();
+        $discount2->setName('Flat Rate 10CHF');
+        $discount2->setMinus(true);
+        $discount2->setPercentage(false);
+        $discount2->setValue(50);
+        $discount2->setUser($manager->merge($this->getReference('default-user')));
+        $this->addReference('flatrate_standarddiscount', $discount2);
+        $manager->persist($discount2);
+
+		$manager->flush();
+	}
+
+	/**
+	 * the order in which fixtures will be loaded
+	 *
+	 * @return int
+	 */
+	public function getOrder()
+	{
+		return 90;
+	}
+}
+
---
+++ Entity/StandardDiscount.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Dime\TimetrackerBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Dime\TimetrackerBundle\Model\DimeEntityInterface;
+
+/**
+ * Class StandardDiscount
+ * @package Dime\TimetrackerBundle\Entity
+ * @ORM\Entity(repositoryClass="Dime\TimetrackerBundle\Entity\StandardDiscountRepository")
+ * @ORM\Table(name="standard_discounts")
+ */
+class StandardDiscount extends Entity implements DimeEntityInterface
+{
+
+	/**
+	 * @var
+	 * @ORM\Column(type="string")
+	 */
+	protected $name;
+
+	/**
+	 * @var
+	 * @ORM\Column(type="decimal", precision=10, scale=2)
+	 */
+	protected $value;
+
+	/**
+	 * @var
+	 * @ORM\Column(type="boolean", nullable=true)
+	 */
+	protected $percentage;
+
+	/**
+	 * @var
+	 * @ORM\Column(type="boolean", nullable=true)
+	 */
+	protected $minus;
+
+	/**
+	 * @return mixed
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * @param mixed $name
+	 *
+	 * @return $this
+	 */
+	public function setName($name)
+	{
+		$this->name = $name;
+		return $this;
+	}
+
+
+	/**
+	 * @return mixed
+	 */
+	public function getMinus()
+	{
+		return $this->minus;
+	}
+
+	/**
+	 * @param mixed $minus
+	 *
+	 * @return $this
+	 */
+	public function setMinus($minus)
+	{
+		$this->minus = $minus;
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getPercentage()
+	{
+		return $this->percentage;
+	}
+
+	/**
+	 * @param mixed $percentage
+	 *
+	 * @return $this
+	 */
+	public function setPercentage($percentage)
+	{
+		$this->percentage = $percentage;
+		return $this;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getValue()
+	{
+		return $this->value;
+	}
+
+	/**
+	 * @param mixed $value
+	 *
+	 * @return $this
+	 */
+	public function setValue($value)
+	{
+		$this->value = $value;
+		return $this;
+	}
+
+	/**
+	 * Returns the Positive or negative integer Value of which shall be subtracted of gross.
+	 *
+	 * @param $gross
+	 *
+	 * @return float|int $amount
+	 */
+	public function getModifier($gross)
+	{
+		$amount = $this->value;
+		if ($this->percentage)
+		{
+			$amount = $this->percentofgross($gross, $this->value, 10);
+		}
+		if($this->minus)
+		{
+			$amount = floatval('-'.$amount);
+		}
+		return $amount;
+	}
+
+	private function percentofgross($gross, $percent, $precision)
+	{
+		$res = round( $percent * $gross, $precision );
+
+		return $res;
+	}
+} 
