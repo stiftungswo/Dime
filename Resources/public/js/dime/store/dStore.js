a0a98fe51071928260d519a5464531d4c88b0a2d
toasterson <toasterson@gmail.com>
2015-03-03 15:01:45 +0100

    Added: Patch functionality to dojo dstore Changed: duplicate URL from get to patch fixed: Money Tests were incorrect due to class moving. Fixed: Requirements for search parameters in controllers did not allow - and other characters while fileds allowed. Fixed: Function in wrong doc section.

--- Resources/public/js/dime/store/dStore.js
+++ Resources/public/js/dime/store/dStore.js
@@ -61,7 +61,80 @@ define([
                 return result;
             });
 
-        }
+        },
 
+        duplicate: function(id, options){
+            options = options || {};
+            var headers = lang.mixin({ Accept: this.accepts }, this.headers, options.headers);
+            var store = this;
+            var initialResponse = request(this.target + '/' + id + '/duplicate', {
+                headers: headers,
+                method: 'PATCH'
+            });
+            return initialResponse.then(function (response) {
+                var event = {};
+
+                var result = event.target = store._restore(store.parse(response), true);
+
+                when(initialResponse.response, function (httpResponse) {
+                    if(httpResponse.status === 200) {
+                        store.emit('add', event);
+                    }
+                });
+                return result;
+            });
+        },
+
+        patch: function (object, options) {
+            // summary:
+            //		Stores an object. This will trigger a PUT request to the server
+            //		if the object has an id, otherwise it will trigger a POST request.
+            // object: Object
+            //		The object to store.
+            // options: __PutDirectives?
+            //		Additional metadata for storing the data.  Includes an 'id'
+            //		property if a specific id is to be used.
+            // returns: dojo/_base/Deferred
+            options = options || {};
+            var id = ('id' in options) ? options.id : this.getIdentity(object);
+            var hasId = typeof id !== 'undefined';
+            var store = this;
+
+            var positionHeaders = 'beforeId' in options
+                ? (options.beforeId === null
+                ? {'Put-Default-Position': 'end'}
+                : {'Put-Before': options.beforeId})
+                : (!hasId || options.overwrite === false
+                ? {'Put-Default-Position': (this.defaultNewToStart ? 'start' : 'end')}
+                : null);
+
+            var initialResponse = request(hasId ? this.target + '/' + id : this.target, {
+                method: 'PATCH',
+                data: this.stringify(object),
+                headers: lang.mixin({
+                    'Content-Type': 'application/json',
+                    Accept: this.accepts,
+                    'If-Match': options.overwrite === true ? '*' : null,
+                    'If-None-Match': options.overwrite === false ? '*' : null
+                }, positionHeaders, this.headers, options.headers)
+            });
+            return initialResponse.then(function (response) {
+                var event = {};
+
+                if ('beforeId' in options) {
+                    event.beforeId = options.beforeId;
+                }
+
+                var result = event.target = store._restore(store.parse(response), true) || object;
+
+                when(initialResponse.response, function (httpResponse) {
+                    if(httpResponse.status === 200) {
+                        store.emit('update', event);
+                    }
+                });
+
+                return result;
+            });
+        }
     });
 });
