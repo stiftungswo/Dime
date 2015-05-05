7665043838b2f3a9109bb8d0ee60410ac40ae87e
toasterson <toasterson@gmail.com>
2015-05-05 16:35:56 +0200

    Changed: Needed to remove any references to dart:mirrors Added: Max Depth to reduce JSON responses and speed up.

--- env/composerupdate.sh
+++ env/composerupdate.sh
@@ -3,5 +3,5 @@ set -x
 SCRIPT=$(realpath ${BASH_SOURCE[0]})
 SCRIPT_PATH=$(dirname ${SCRIPT})
 sudo chmod -R 777 /tmp/app
-composer.phar update
+composer.phar update $@
 ${SCRIPT_PATH}/cacheclear.sh
