68f9701a40f419cb0bc65c69954990a97847193b
Demian Holderegger <demian.holderegger@gmail.com>
2015-07-02 10:59:41 +0200

    store pub_cache in vagrant folder instead of home

--- env/pubInstall.sh
+++ env/pubInstall.sh
@@ -5,8 +5,7 @@ SCRIPT_PATH=$(dirname ${SCRIPT})
 APP_ROOT=$(realpath ${SCRIPT_PATH}/..)
 
 wget 'https://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/dartsdk-linux-x64-release.zip' -O /tmp/dart-sdk.zip --no-verbose
-sudo unzip -d /usr/local/ /tmp/dart-sdk.zip
+sudo unzip -o -d /usr/local/ /tmp/dart-sdk.zip
 sudo chmod -R a+rx /usr/local/dart-sdk
 rm /tmp/dart-sdk.zip
-sudo chown -R apache:apache $(getent passwd apache | awk '{split($0,a,":"); print a[6]}')
 $SCRIPT_PATH/pub.sh update
