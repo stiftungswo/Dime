22305789263f9ee1c28607c29958b1905ab857ed
Demian Holderegger <demian.holderegger@gmail.com>
2015-07-02 14:24:50 +0200

    updating submodules

--- env/shell/user.sh
+++ env/shell/user.sh
@@ -3,6 +3,7 @@ WEBROOT=$1
 COMPOSERDIR=$2
 cd $WEBROOT
 php $COMPOSERDIR/composer.phar install
+mysql -u dime -pdime -e "DROP DATABASE dime; CREATE DATABASE dime;"
 php app/console doctrine:schema:create
 php app/console assetic:dump
 php app/console h4cc_alice_fixtures:load:sets
