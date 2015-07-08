2d91d3fb3c0fb6ac0940807820ec361bdaca027a
Demian Holderegger <demian.holderegger@gmail.com>
2015-07-08 08:57:43 +0200

    loading fixtures from sql dump instead of alice
    should work on travis and also be more stable when testing

--- env/shell/user.sh
+++ env/shell/user.sh
@@ -5,7 +5,7 @@ cd $WEBROOT
 php $COMPOSERDIR/composer.phar install
 mysql -u dime -pdime -e "DROP DATABASE dime; CREATE DATABASE dime;"
 php app/console doctrine:schema:create
+mysql -u dime -pdime dime < env/fixtures/dime.sql
 php app/console assetic:dump
-php app/console h4cc_alice_fixtures:load:sets
 php app/console asset:install --symlink
 /vagrant/env/pubInstall.sh
