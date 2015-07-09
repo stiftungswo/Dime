e9ad1bf809e30230eff54f1218ba7a8e771869e0
toasterson <toasterson@gmail.com>
2015-07-09 09:44:39 +0200

    Dime-19 Menu is visible when not logged in Fixes #19

--- Resources/public/lib/component/main/menu.dart
+++ Resources/public/lib/component/main/menu.dart
@@ -2,6 +2,7 @@ library main_menu;
 
 import 'package:angular/angular.dart';
 import 'package:DimeClient/model/menu.dart';
+import 'package:DimeClient/service/user_auth.dart';
 
 
 @Component(
@@ -9,7 +10,9 @@ import 'package:DimeClient/model/menu.dart';
     templateUrl: '/bundles/dimefrontend/packages/DimeClient/component/main/menu.html',
     useShadowDom: false)
 class MenuComponent implements ScopeAware {
-  
+  MenuComponent(this.auth);
+
+  UserAuthProvider auth;
   Scope scope;
 
   bool oneAtATime = false;
