0b730cb6d4ce520923b44045c620487dcbd86bec
Demian Holderegger <demian.holderegger@gmail.com>
2015-07-24 14:05:37 +0200

    prepare app for deployment in production/test mode

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -5,83 +5,69 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 class AppKernel extends Kernel
 {
 
-    public function registerBundles()
-    {
-        $bundles = array(
-            
-            // symfony-standard edition
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            
-            // vendor packages
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
-            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-            new Ps\PdfBundle\PsPdfBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
-            new FOS\RestBundle\FOSRestBundle(),
-            new JMS\SerializerBundle\JMSSerializerBundle($this),
-            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
-            new \Knp\JsonSchemaBundle\KnpJsonSchemaBundle(),
-            new \Swo\CommonsBundle\SwoCommonsBundle(),
-            new \Misd\PhoneNumberBundle\MisdPhoneNumberBundle(),
-            new \Tbbc\MoneyBundle\TbbcMoneyBundle(),
+	public function registerBundles()
+	{
+		$bundles = array(
 
-            //Own Bundles
-            new Dime\PrintingBundle\DimePrintingBundle(),
-            new Dime\FrontendBundle\DimeFrontendBundle(),
-            new Dime\OfferBundle\DimeOfferBundle(),
-            new Dime\InvoiceBundle\DimeInvoiceBundle(),
-            new Dime\EmployeeBundle\DimeEmployeeBundle(),
-            new Dime\ReportBundle\DimeReportBundle(),
-            new Dime\TimetrackerBundle\DimeTimetrackerBundle(),
-        );
-        
-        if (in_array($this->getEnvironment(), array(
-            'dev',
-            'test'
-        ))) {
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new h4cc\AliceFixturesBundle\h4ccAliceFixturesBundle();
-        }
-        
-        return $bundles;
-    }
+			// symfony-standard edition
+			new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+			new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+			new Symfony\Bundle\TwigBundle\TwigBundle(),
+			new Symfony\Bundle\MonologBundle\MonologBundle(),
+			new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+			new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+			new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+			new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
-    }
+			// vendor packages
+			new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+			new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+			new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+			new Ps\PdfBundle\PsPdfBundle(),
+			new FOS\UserBundle\FOSUserBundle(),
+			new FOS\RestBundle\FOSRestBundle(),
+			new JMS\SerializerBundle\JMSSerializerBundle($this),
+			new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+			new \Knp\JsonSchemaBundle\KnpJsonSchemaBundle(),
+			new \Swo\CommonsBundle\SwoCommonsBundle(),
+			new \Misd\PhoneNumberBundle\MisdPhoneNumberBundle(),
+			new \Tbbc\MoneyBundle\TbbcMoneyBundle(),
 
-    public function getCacheDir()
-    {
-        if (in_array($this->getEnvironment(), array(
-            'dev',
-            'test'
-        ))) {
-            return '/dev/shm/appname/cache/' . $this->rootDir . '/' . $this->environment;
-        } else {
-            return $this->rootDir . '/cache/' . $this->environment;
-        }
-    }
+			//Own Bundles
+			new Dime\PrintingBundle\DimePrintingBundle(),
+			new Dime\FrontendBundle\DimeFrontendBundle(),
+			new Dime\OfferBundle\DimeOfferBundle(),
+			new Dime\InvoiceBundle\DimeInvoiceBundle(),
+			new Dime\EmployeeBundle\DimeEmployeeBundle(),
+			new Dime\ReportBundle\DimeReportBundle(),
+			new Dime\TimetrackerBundle\DimeTimetrackerBundle(),
+		);
 
-    public function getLogDir()
-    {
-        if (in_array($this->getEnvironment(), array(
-            'dev',
-            'test'
-        ))) {
-            return '/dev/shm/appname/log/' . $this->rootDir . '/' . $this->environment;
-        } else {
-            return $this->rootDir . '/logs/' . $this->environment;
-        }
-    }
+		if (in_array($this->getEnvironment(), array(
+			'dev',
+			'test'
+		))) {
+			$bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+			$bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+			$bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+			$bundles[] = new h4cc\AliceFixturesBundle\h4ccAliceFixturesBundle();
+		}
+
+		return $bundles;
+	}
+
+	public function registerContainerConfiguration(LoaderInterface $loader)
+	{
+		$loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
+	}
+
+	public function getCacheDir()
+	{
+		return '/dev/shm/dime/cache/' . $this->rootDir . '/' . $this->environment;
+	}
+
+	public function getLogDir()
+	{
+		return '/dev/shm/dime/log/' . $this->rootDir . '/' . $this->environment;
+	}
 }
