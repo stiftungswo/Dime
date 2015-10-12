1d0f99bf06e5ad46349ff4ca9b0f60bf5b7c0b16
Gautham Arumugam <gaut1993@gmail.com>
2015-10-12 08:55:35 +0200

    add synced folder config for windows in vagrant

--- env/puppet/manifests/00_main.pp
+++ env/puppet/manifests/00_main.pp
@@ -1,5 +1,5 @@
 Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
-File { owner => 0, group => 0, mode => 0644 }
+File { owner => 0, group => 0, mode => '0644' }
 stage { 'first': }
 stage { 'last': }
 Stage['first'] -> Stage['main'] -> Stage['last']
