0edcae421f9d48940d36f9b9b226faf60a48d302
Demian Holderegger <demian.holderegger@gmail.com>
2015-10-13 15:17:23 +0200

    remove puppet config

--- env/puppet/manifests/00_main.pp
+++
@@ -1,9 +0,0 @@
-Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
-File { owner => 0, group => 0, mode => '0644' }
-stage { 'first': }
-stage { 'last': }
-Stage['first'] -> Stage['main'] -> Stage['last']
-
-class{'basic':
-  stage => first
-}
