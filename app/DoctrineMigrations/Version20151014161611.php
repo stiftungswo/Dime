ce994a45ad52fbee129c9e0ee0bd39ff7c223b38
Gautham Arumugam <gaut-3@hotmail.com>
2015-10-15 13:31:58 +0200

    change create statement

--- app/DoctrineMigrations/Version20151014161611.php
+++ app/DoctrineMigrations/Version20151014161611.php
@@ -18,7 +18,7 @@ class Version20151014161611 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE area_of_activity (id INT(255) NOT NULL, areaOfActivity VARCHAR(255) DEFAULT NULL,created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, INDEX IDX_71C44DB0A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE area_of_activity(id INT(6) UNSIGNED PRIMARY KEY NOT NULL, areaOfActivity VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
 
 
     }
