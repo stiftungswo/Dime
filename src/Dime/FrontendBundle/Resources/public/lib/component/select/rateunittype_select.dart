cee64c026307d1b34030ced3dc61c4bbdc2e32f3
Patrick <patschmid11@bluewin.ch>
2015-10-23 09:02:15 +0200

    entity_select splitted

---
+++ src/Dime/FrontendBundle/Resources/public/lib/component/select/rateunittype_select.dart
@@ -0,0 +1,17 @@
+part of entity_select;
+
+
+@Component(
+    selector: 'rateunittype-select',
+    templateUrl: '/bundles/dimefrontend/packages/DimeClient/component/select/rateunittype_select.html',
+    useShadowDom: false,
+    map: const{
+      'rateunittype': '<=>selectedEntity',
+      'callback': '&callback',
+      'field': '=>!field',
+      'clearOnClose': '=>!clearOnClose'
+    }
+)
+class RateUnitTypeSelectComponent extends EntitySelect {
+  RateUnitTypeSelectComponent(DataCache store, dom.Element element, StatusService status, UserAuthProvider auth): super(RateUnitType, store, element, status, auth);
+}
