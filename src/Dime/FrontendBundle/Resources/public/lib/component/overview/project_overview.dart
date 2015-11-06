6e2ae78e0f7c8fa5a9e788505090999797ca04db
Patrick <patschmid11@bluewin.ch>
2015-11-06 09:15:45 +0100

    user_select useContext retrieved, current user set as accountant when creating offer/project

--- src/Dime/FrontendBundle/Resources/public/lib/component/overview/project_overview.dart
+++ src/Dime/FrontendBundle/Resources/public/lib/component/overview/project_overview.dart
@@ -6,15 +6,19 @@ part of entity_overview;
     useShadowDom: false
 )
 class ProjectOverviewComponent extends EntityOverview {
-  ProjectOverviewComponent(DataCache store, Router router, SettingsManager manager, StatusService status, UserAuthProvider auth, RouteProvider prov):
+  ProjectOverviewComponent(DataCache store, this.context, Router router, SettingsManager manager, StatusService status, UserAuthProvider auth, RouteProvider prov):
   super(Project, store, 'project_edit', manager, status, auth: auth, router: router);
 
   String sortType = "name";
+  UserContext context;
 
   cEnt({Project entity}) {
     if (entity != null) {
       return new Project.clone(entity);
     }
-    return new Project();
+    Project newProject = new Project();
+    newProject.accountant = this.context.employee;
+    newProject.addFieldtoUpdate('accountant');
+    return newProject;
   }
 }
