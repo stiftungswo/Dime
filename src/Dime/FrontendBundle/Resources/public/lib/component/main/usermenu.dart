952207dee0725219df8bde48c676cdcd69ca7c65
Demian Holderegger <demian.holderegger@gmail.com>
2015-12-24 11:28:06 +0100

    new user menu

--- src/Dime/FrontendBundle/Resources/public/lib/component/main/usermenu.dart
+++ src/Dime/FrontendBundle/Resources/public/lib/component/main/usermenu.dart
@@ -2,16 +2,25 @@ library dime.usermenu;
 
 import 'package:angular/angular.dart';
 import 'package:DimeClient/service/user_auth.dart';
+import 'package:DimeClient/service/user_context.dart';
 
 @Component(selector: 'usermenu', templateUrl: '/bundles/dimefrontend/packages/DimeClient/component/main/usermenu.html', useShadowDom: false)
 class UserMenu {
   UserAuthProvider auth;
 
+  UserContext userContext;
+
+  Router router;
+
   bool get isLoggedIn => auth.isloggedin;
 
-  UserMenu(this.auth);
+  UserMenu(this.auth, this.userContext, this.router);
 
   logout() async {
     await this.auth.logout();
+  }
+
+  userEditor() {
+    router.go("employee_edit", {'id': userContext.employee.id});
   }
 }
