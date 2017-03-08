c92739216ca92d040c1586800e7e0304514b3dde
Demian Holderegger <demian.holderegger@gmail.com>
2017-03-08 15:40:48 +0100

    pub build does not have a --packages-dir argument

--- env/pubRebuild.sh
+++ env/pubRebuild.sh
@@ -5,4 +5,4 @@ SCRIPT_PATH=$(dirname ${SCRIPT})
 APP_ROOT=$(realpath ${SCRIPT_PATH}/..)
 
 rm -rf $APP_ROOT/src/Dime/FrontendBundle/Resources/public/build
-$SCRIPT_PATH/pub.sh build --packages-dir
+$SCRIPT_PATH/pub.sh build
