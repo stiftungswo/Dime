1c0300f6b1500e507779056b7bec225ec80204ad
Demian Holderegger <demian.holderegger@gmail.com>
2017-03-22 09:40:05 +0100

    put/delete period tests

--- src/Dime/TimetrackerBundle/Tests/Controller/PeriodControllerTest.php
+++ src/Dime/TimetrackerBundle/Tests/Controller/PeriodControllerTest.php
@@ -46,8 +46,6 @@ class PeriodControllerTest extends DimeTestCase
         $this->loginAs('admin');
         /* create new period */
         $response = $this->jsonRequest('POST', $this->api_prefix.'/periods', json_encode(array(
-            'description'   => 'Period test description',
-            'customer'      => 1,
             'user'          => 1,
             'employee'      => 7,
             "start"         => "2017-01-25 00:00:00",
@@ -72,23 +70,25 @@ class PeriodControllerTest extends DimeTestCase
         $this->assertEquals('2017-01-25 00:00:00', $data['start']);
         $this->assertEquals('0.8', $data['pensum']);
 
-        /*
+
         // modify period
         $response = $this->jsonRequest('PUT', $this->api_prefix.'/periods/' . $id, json_encode(array(
-            'name'          => 'Modified Test',
-            'alias'         => 'test2',
-            'description'   => 'Period test description update',
-            'customer'      => 1,
-            'user'          => 1
+            'user'          => 4,
+            'employee'      => 3,
+            "start"         => "2017-02-28 00:00:00",
+            "end"           => "2017-08-31 00:00:00",
+            "pensum"        => 1,
+            "lastYearHolidayBalance" => 22
         )));
         $this->assertEquals(200, $response->getStatusCode());
 
         $response = $this->jsonRequest('PUT', $this->api_prefix.'/periods/' . ($id+100), json_encode(array(
-            'name'          => 'Modified Test',
-            'alias'         => 'test',
-            'description'   => 'Period test description update',
-            'customer'      => 1,
-            'user'          => 1
+            'user'          => 4,
+            'employee'      => 3,
+            "start"         => "2017-02-28 00:00:00",
+            "end"           => "2017-08-31 00:00:00",
+            "pensum"        => 1,
+            "lastYearHolidayBalance" => 22
         )));
         $this->assertEquals(404, $response->getStatusCode());
 
@@ -99,8 +99,9 @@ class PeriodControllerTest extends DimeTestCase
         $data = json_decode($response->getContent(), true);
 
         // assert that data has content
-        $this->assertEquals('Modified Test', $data['name'], 'expected to find "Modified Test"');
-        $this->assertEquals('test2', $data['alias'], 'expected to find alias "test2"');
+        $this->assertEquals('2017-02-28 00:00:00', $data['start']);
+        $this->assertEquals(1, $data['pensum']);
+        $this->assertEquals(22, $data['lastYearHolidayBalance']);
 
         // delete period
         $response = $this->jsonRequest('DELETE', $this->api_prefix.'/periods/' . $id);
@@ -109,6 +110,5 @@ class PeriodControllerTest extends DimeTestCase
         // check if period still exists
         $response = $this->jsonRequest('GET', $this->api_prefix.'/periods/' . $id);
         $this->assertEquals(404, $response->getStatusCode());
-        */
     }
 }
