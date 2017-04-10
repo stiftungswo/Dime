3291ca4cf25903c058477dea18584f058bb220f7
Pascal Kaufmann <kaufmae@me.com>
2017-04-10 16:00:34 +0200

    switching to docker-compose powered mode

---
+++ .docker/rootfs/entrypoint.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+# run startup commands
+composer install
+
+mysql --host=mysql -u root -e "CREATE DATABASE IF NOT EXISTS dime;"
+mysql --host=mysql -u root -e "GRANT all privileges on dime.* to dime@'%' IDENTIFIED BY 'dime';"
+mysql --host=mysql -u root -e "FLUSH PRIVILEGES;"
+mysql --host=mysql -u root -e "DROP DATABASE IF EXISTS dime; CREATE DATABASE dime;"
+php app/console doctrine:schema:create
+mysql --host=mysql -u root dime < ./env/fixtures/dime.sql
+cp ./app/config/parameters.yml.dist ./app/config/parameters.yml
+
+php app/console assetic:dump
+php app/console asset:install --symlink
+
+cd ./src/Dime/FrontendBundle/Resources/public
+pub get --packages-dir
+
+
+# update supervisor configuration
+
+if [ -f /var/www/html/.docker/supervisord.conf ]; then
+	cp /var/www/html/.docker/supervisord.conf /var/www/html/supervisord.conf
+fi
+
+if [ -f /var/www/html/supervisord.conf ]; then
+	echo "===> detected supervisord.conf in project root"
+	echo -e "\n" >> /etc/supervisor/supervisord.conf
+    cat /var/www/html/supervisord.conf >> /etc/supervisor/supervisord.conf
+fi
+
+
+# setup cron
+
+if [ `whoami` = 'root' ]; then
+    if [ "$GIT_BRANCH" == 'develop' ] || [ "$GIT_BRANCH" == 'master' ]; then
+        if [ -f /var/www/html/.docker/crontab ]; then
+            cp /var/www/html/.docker/crontab /var/www/html/crontab
+        fi
+
+        if [ -f /var/www/html/crontab ]; then
+            echo "===> detected crontab in project root"
+            crontab /var/www/html/crontab
+            crontab -l
+            echo "" >> /etc/supervisor/supervisord.conf
+            echo "[program:cron]" >> /etc/supervisor/supervisord.conf
+            echo "command=cron -f -L 15" >> /etc/supervisor/supervisord.conf
+            echo "autostart=true" >> /etc/supervisor/supervisord.conf
+            echo "autorestart=true" >> /etc/supervisor/supervisord.conf
+        fi
+    fi
+fi
+
+echo -e "===> generated supervisord configuration:\n"
+cat /etc/supervisor/supervisord.conf
+echo -e "\n\n===> starting supervisord..."
+
+
+# start supervisord!
+
+/usr/bin/supervisord -c /etc/supervisor/supervisord.conf
--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,43 @@
-FROM php:7.0-apache
-COPY . /dime
-COPY env/conf/dime.docker.conf /etc/apache2/conf-enabled/dime.docker.conf
+FROM php:7.1.3-fpm
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+RUN apt-get update -y && \
+    apt-get install -qqy nginx supervisor git locales libssl-dev libmcrypt-dev libicu-dev openvpn curl links cron redis-tools mysql-client vim && \
+    echo "cs_CZ.UTF-8 UTF-8" > /etc/locale.gen && locale-gen cs_CZ.UTF-8 && \
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen && locale-gen en_US.UTF-8 && \
+    dpkg-reconfigure locales && \
+    pecl install mongodb && docker-php-ext-enable mongodb && \
+    pecl install redis && docker-php-ext-enable redis && \
+    docker-php-ext-install bcmath mbstring intl iconv mcrypt zip mysqli pdo pdo_mysql opcache && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /var/www/html/* && \
+    touch /run/nginx.pid && touch /run/php-fpm.pid && touch /run/supervisord.pid && touch /var/run/crond.pid && \
+    chown -R www-data:www-data /var/www /var/lib/nginx /run/nginx.pid /run/php-fpm.pid /run/supervisord.pid /var/run/crond.pid && \
+    php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php && \
+    php composer-setup.php && \
+    php -r "unlink('composer-setup.php');" && \
+    mv composer.phar /usr/local/bin/composer
+
+RUN apt-get update && \
+    apt-get install -y php-apc apt-transport-https && \
+    curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
+    curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list && \
+    apt-get update && \
+    apt-get install dart && \
+    ulimit -n 16384
+
+COPY .docker/rootfs /
+COPY ./env/config/dime.ini /usr/local/etc/php/conf.d/
+
+RUN chown www-data:www-data /etc/supervisor/supervisord.conf && \
+    chmod +x /entrypoint.sh && \
+    mkdir -p /var/www/.composer && \
+    chmod -R 777 /var/www/.composer && \
+    chown -R www-data:www-data /var/www/*
+
+WORKDIR /var/www/html
+
+EXPOSE 8080
+
+CMD ["/entrypoint.sh"]
