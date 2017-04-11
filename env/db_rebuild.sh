eb7bb3b904a581342b76c396c1cecc284f8a5418
Pascal Kaufmann <kaufmae@me.com>
2017-04-11 11:01:53 +0200

    clean up some files, remove some comments and kill some zombies

--- env/db_rebuild.sh
+++ env/db_rebuild.sh
@@ -5,7 +5,7 @@ set -x
 $ROOT_DIR/env/fixtures/flush_db.sh
 
 # rebuild the schema
-php ${ROOT_DIR}/app/console doc:data:drop --force
-php ${ROOT_DIR}/app/console doc:data:create
+# php ${ROOT_DIR}/app/console doc:data:drop --force
+# php ${ROOT_DIR}/app/console doc:data:create
 php ${ROOT_DIR}/app/console doc:schema:create
-php ${ROOT_DIR}/app/console doc:fix:load -n
+# php ${ROOT_DIR}/app/console doc:fix:load -n
