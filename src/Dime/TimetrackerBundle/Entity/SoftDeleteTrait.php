e6cefa3b7cd93e08b9fdc7af17a44073590f9562
Michael Schär <mdschaer@gmail.com>
2017-09-06 11:17:32 +0200

    Soft Delete Trait & Migration file

---
+++ src/Dime/TimetrackerBundle/Entity/SoftDeleteTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Dime\TimetrackerBundle\Entity;
+
+trait SoftDeleteTrait
+{
+
+  /**
+    * @var DateTime $deletedAt
+  * @JMS\SerializedName("deletedAt")
+    * @Assert\Date()
+    * @ORM\Column(name="deleted_at", type="datetime", nullable=true)
+    */
+    protected $deletedAt;
+
+  /**
+    * Get deletedAt
+    * @return datetime
+    */
+    public function getDeletedAt()
+    {
+            return $this->deletedAt;
+    }
+
+    /**
+     * Set deletedAt
+     * @param \DateTime $deletedAt
+     * @return Project
+     */
+    public function setDeletedAt($deletedAt)
+    {
+            $this->deletedAt = $deletedAt;
+    }
+}
