550180fa4c8058f784338b3295ce29f5c6bc130e
Michael Schär <mdschaer@gmail.com>
2017-09-06 11:44:39 +0200

    BD scripts

--- db_scripts/moveTimeslicesToProject.sql
+++ db_scripts/moveTimeslicesToProject.sql
@@ -3,8 +3,8 @@
 /* Define Project id's, start and end date */
 SET @currentProjectId=1, @newProjectId=2, @startDate:='2017-01-01', @endDate:='2017-10-01';
 
-/* TODO JOIN activities */
-UPDATE timeslices 
-SET activity = @newActivity 
+/* TODO JOIN activities / services??? */
+UPDATE timeslices
+SET activity = @newActivity
 WHERE activity = @currentActivity
 AND started_at >= @startDate AND stopped_at < @endDate;
