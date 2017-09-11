d28976efbc85bb6140f9319b8f3a556de9ba279c
Michael Schär <mdschaer@gmail.com>
2017-09-11 10:35:40 +0200

    App.php: Removed APC caching because of caching issues on prod

--- web/app.php
+++ web/app.php
@@ -1,6 +1,6 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
+//use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
@@ -8,16 +8,19 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
+
+/*
+// APC commented out because of caching problems
 $apcLoader = new ApcClassLoader('DimeTimetracker', $loader);
 $loader->unregister();
-$apcLoader->register(true);
+$apcLoader->register(true);*/
 
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+require_once __DIR__.'/../app/AppCache.php'; // don't use this with apc
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+$kernel = new AppCache($kernel); // don't use this with apc
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
