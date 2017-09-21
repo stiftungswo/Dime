adc515abec9a12f0812d099a3ce5247a376fd51b
Michael Schär <mdschaer@gmail.com>
2017-09-21 15:13:01 +0200

    Timeslice performance improvements, NewEntryDate simlification, frontend build

--- src/Dime/FrontendBundle/Resources/public/lib/component/select/user_select.dart
+++ src/Dime/FrontendBundle/Resources/public/lib/component/select/user_select.dart
@@ -18,17 +18,36 @@ class UserSelectComponent extends EntitySelect {
   UserContext context;
   bool useContext = false;
 
-  set selectedEntity(entity) {
-    _selectedEntity = entity;
-    selector = EntText;
-  }
+  @NgOneWay('parent-employees')
+  List<Activity> parentEmployees = null;
+
+  // Disable the select box because of projectId being null sometimes
+  @NgOneWayOneTime('is-readonly')
+  bool isReadonly = false;
 
   get EntText => _selectedEntity != null ? _selectedEntity.fullname : '';
+
+  @override
+  void set scope(Scope scope) {
+    scope.watch('parentEmployees', (newval, oldval) => onChange(oldval, newval));
+  }
+
+  onChange(List oldList, List newList) {
+    if(this.entities != null && this.entities.length == 0
+          && newList != null && newList.length > 0) {
+      reload();
+    }
+  }
 
   reload() async{
     this.statusservice.setStatusToLoading();
     try {
-      this.entities = (await this.store.list(Employee, params: {"enabled": 1})).toList();
+      if(this.parentEmployees != null) {
+        this.entities = this.parentEmployees;
+      }
+      else {
+        this.entities = (await this.store.list(Employee, params: {"enabled": 1})).toList();
+      }
       this.statusservice.setStatusToSuccess();
     } catch (e) {
       this.statusservice.setStatusToError(e);
