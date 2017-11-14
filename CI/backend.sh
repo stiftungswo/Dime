d86a9ea09c2efd7b54c341391f58c9c61e7ebd7a
jgschwend <gschwendlj@hotmail.com>
2017-11-14 11:43:05 +0100

    Create the database using doctrine instead of bash

--- CI/backend.sh
+++ CI/backend.sh
@@ -1,8 +1,7 @@
 #!/bin/bash -x
-mysql -e 'create database dime;' && \
 cp app/config/parameters.yml.travis app/config/parameters.yml.dist && \
 composer install -n && \
-php app/console doctrine:schema:create && \
+php app/console doctrine:database:create && \
 php app/console --no-interaction doctrine:migrations:migrate && \
 mysql dime < env/fixtures/dime.sql && \
 php app/console assetic:dump && \
