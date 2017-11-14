d03de0a7314de247cfab6ba033bc71c90000e8fa
jgschwend <gschwendlj@hotmail.com>
2017-11-14 11:32:00 +0100

    Avoid that doctrine asks whether to migrate

--- CI/backend.sh
+++ CI/backend.sh
@@ -3,7 +3,7 @@ mysql -e 'create database dime;' && \
 cp app/config/parameters.yml.travis app/config/parameters.yml.dist && \
 composer install -n && \
 php app/console doctrine:schema:create && \
-php app/console doctrine:migrations:migrate && \
+php app/console --no-interaction doctrine:migrations:migrate && \
 mysql dime < env/fixtures/dime.sql && \
 php app/console assetic:dump && \
 php app/console asset:install --symlink --relative
