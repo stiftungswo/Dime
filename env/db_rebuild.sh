a303c60945bb709ffd7d919fe23947028d606900
jgschwend <gschwendlj@hotmail.com>
2017-11-15 17:12:50 +0100

    Create create database and migrate instead of create schema

--- env/db_rebuild.sh
+++ env/db_rebuild.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 set -x
 
@@ -7,7 +7,8 @@ $ROOT_DIR/env/fixtures/flush_db.sh
 # rebuild the schema
 # php ${ROOT_DIR}/app/console doc:data:drop --force
 # php ${ROOT_DIR}/app/console doc:data:create
-php ${ROOT_DIR}/app/console doc:schema:create
+php ${ROOT_DIR}/app/console doctrine:database:create
+php ${ROOT_DIR}/app/console doctrine:migrations:migrate --verbose
 # php ${ROOT_DIR}/app/console doc:fix:load -n
 
-$ROOT_DIR/env/fixtures/load.sh
+#$ROOT_DIR/env/fixtures/load.sh
