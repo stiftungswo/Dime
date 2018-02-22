bee0a512bcc273a1604deaa62b0ae4e4b29f9221
Paul Sonnenschein <paulsonnenschein0@gmail.com>
2018-02-22 12:16:20 +0100

    Add prettier / dartfmt cofig and documentation [ci skip]

--- ci/check_format.sh
+++ ci/check_format.sh
@@ -1,4 +1,4 @@
-# checks if formatters could have been run locally to fix the code style 
+# checks if formatters could have been run locally to fix the code style
 
 # check backend
 composer global require "squizlabs/php_codesniffer=*"
@@ -10,7 +10,7 @@ code1=$?
 $HOME/.composer/vendor/bin/phpcs --standard=psr2 --extensions=php --ignore=src/Tbbc/MoneyBundle,src/Money,src/Dime/FrontendBundle/Resources/public src > /dev/null 2>&1
 code2=$?
 if [ $code1 -ne $code2 ]; then
-  echo "PHP backend is not properly formatted. Please reformat and commit again. See #TODO"
+  echo "PHP backend is not properly formatted. Please reformat and commit again. See https://github.com/stiftungswo/Dime/tree/master#formatting"
   exit 1;
 fi
 
@@ -18,6 +18,6 @@ fi
 cd src/Dime/FrontendBundle/Resources/public
 ../../../../../dart-sdk/bin/dartfmt -n --line-length 140 --set-exit-if-changed lib/
 if [ $? -ne 0 ]; then
-  echo "Dart frontend is not properly formatted. Please reformat and commit again. See #TODO"
+  echo "Dart frontend is not properly formatted. Please reformat and commit again. See https://github.com/stiftungswo/Dime/tree/master#formatting"
   exit 1;
 fi
