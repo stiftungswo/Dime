d5fdc5c90fd54f9c349a97417c20d1926bf9e0e3
Daniel Latzer <latzer.daniel@gmail.com>
2018-02-26 11:28:55 +0100

    Fix database name not being prepended to db backup

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -42,6 +42,6 @@ ssh $TARGET mv $TMP $PROJECT_DIR
 ssh $TARGET "cd $PROJECT_DIR && php72 app/console cache:clear --env=prod"
 
 ssh $TARGET mkdir -p $BACKUP_DIR
-ssh $TARGET "mysqldump -u $DATABASE_USER -p\"$DATABASE_PW\" $DATABASE | bzip2 -c > $BACKUP_DIR/$DATABASE_$(date +%Y-%m-%d-%H.%M.%S).sql.bz2"
+ssh $TARGET "mysqldump -u $DATABASE_USER -p\"$DATABASE_PW\" $DATABASE | bzip2 -c > $BACKUP_DIR/${DATABASE}_$(date +%Y-%m-%d-%H.%M.%S).sql.bz2"
 ssh $TARGET "cd $PROJECT_DIR && php72 app/console doctrine:migrations:migrate --no-interaction"
 
