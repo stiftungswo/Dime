298d1f8e98ec81efe8c28b7d0295790b22a3cfb3
Paul Sonnenschein <paulsonnenschein0@gmail.com>
2018-03-15 09:15:20 +0100

    use the old percent_input logic to convert percents to floats and back

--- src/Dime/FrontendBundle/Resources/public/lib/src/component/elements/discount_input.dart
+++ src/Dime/FrontendBundle/Resources/public/lib/src/component/elements/discount_input.dart
@@ -1,55 +1,53 @@
 import 'dart:async';
+
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
+
+import 'percentage_input.dart';
 
 @Component(
   selector: "discount-input",
   template: """
     <div class="input-group">
-      <input type="number" [(ngModel)]="displayValue" class="form-control" aria-label="...">
+      <percentage-input *ngIf="percentage" [(value)]="displayValue" [showAddon]="false"></percentage-input>
+      <input *ngIf="!percentage" type="number" [(ngModel)]="displayValue" class="form-control" aria-label="...">
       <div class="input-group-btn">
         <button type="button" class="btn" [class.btn-default]='!percentage' [class.btn-primary]="percentage" (click)='percentage = true'>%</button>
         <button type="button" class="btn" [class.btn-default]='percentage' [class.btn-primary]="!percentage" (click)='percentage = false'>CHF</button>
       </div>
     </div>
   """,
-  directives: const[formDirectives, CORE_DIRECTIVES]
-
+  directives: const [formDirectives, CORE_DIRECTIVES, PercentageInputComponent],
 )
-class DiscountInputComponent{
-
+class DiscountInputComponent {
   bool _percentage;
   bool get percentage => _percentage;
-  @Input() void set percentage(bool percentage) {
+  @Input()
+  void set percentage(bool percentage) {
     _percentage = percentage;
     _percentageChange.add(percentage);
   }
+
   final StreamController<bool> _percentageChange = new StreamController<bool>();
   @Output()
   Stream<bool> get percentageChange => _percentageChange.stream;
 
-  @Input() num value;
+  @Input()
+  num value;
   final StreamController<num> _valueChange = new StreamController<num>();
   @Output()
   Stream<num> get valueChange => _valueChange.stream;
 
-  num get displayValue{
-    if(value == null){
+  num get displayValue {
+    if (value == null) {
       return null;
     }
-    if(percentage){
-      return value * 100;
-    } else {
-      return value;
-    }
+
+    return value;
   }
 
   void set displayValue(num displayValue) {
-    if(percentage){
-      value = displayValue / 100;
-    } else {
-      value = displayValue;
-    }
+    value = displayValue;
     _valueChange.add(value);
   }
 }
