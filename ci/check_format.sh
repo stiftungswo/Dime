b09854a9124ddd709a8cf6c0664a56af06651fa6
Paul Sonnenschein <paulsonnenschein0@gmail.com>
2018-03-23 13:59:35 +0100

    Fix check_format.sh script

--- ci/check_format.sh
+++ ci/check_format.sh
@@ -6,9 +6,9 @@ composer global require "squizlabs/php_codesniffer=*"
 # we run phpcbf twice to check if it was able to fix something
 # since it reports more than it can fix we check the status code twice and check if it changed
 # sadly there is no unique status code for "phpcbf fixed something and maybe there are unfixable errors"
-$HOME/.composer/vendor/bin/phpcs --standard=psr2 --extensions=php --ignore=src/Tbbc/MoneyBundle,src/Money,src/Dime/FrontendBundle/Resources/public src
+$HOME/.composer/vendor/bin/phpcbf --standard=psr2 --extensions=php --ignore=src/Tbbc/MoneyBundle,src/Money,src/Dime/FrontendBundle/Resources/public src
 code1=$?
-$HOME/.composer/vendor/bin/phpcs --standard=psr2 --extensions=php --ignore=src/Tbbc/MoneyBundle,src/Money,src/Dime/FrontendBundle/Resources/public src > /dev/null 2>&1
+$HOME/.composer/vendor/bin/phpcbf --standard=psr2 --extensions=php --ignore=src/Tbbc/MoneyBundle,src/Money,src/Dime/FrontendBundle/Resources/public src > /dev/null 2>&1
 code2=$?
 if [ $code1 -ne $code2 ]; then
   echo "PHP backend is not properly formatted. Please reformat and commit again. See https://github.com/stiftungswo/Dime/tree/master#formatting"
