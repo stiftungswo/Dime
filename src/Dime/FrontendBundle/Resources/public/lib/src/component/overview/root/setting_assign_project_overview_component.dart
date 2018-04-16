8d99243ad838f68fd32a47ff5e733ccb5664b72e
Daniel Latzer <latzer.daniel@gmail.com>
2018-04-16 09:54:00 +0200

    Split overviews into 'root' and 'child' directories (#124)

--- src/Dime/FrontendBundle/Resources/public/lib/src/component/overview/root/setting_assign_project_overview_component.dart
+++ src/Dime/FrontendBundle/Resources/public/lib/src/component/overview/root/setting_assign_project_overview_component.dart
@@ -3,17 +3,17 @@ import 'dart:async';
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
 
-import '../../model/entity_export.dart';
-import '../../service/caching_object_store_service.dart';
-import '../../service/entity_events_service.dart';
-import '../../service/settings_service.dart';
-import '../../service/status_service.dart';
-import '../../service/user_auth_service.dart';
-import '../../service/user_context_service.dart';
-import '../../util/page_title.dart' as page_title;
-import '../common/dime_directives.dart';
-import '../select/select.dart';
-import 'entity_overview.dart';
+import '../../../model/entity_export.dart';
+import '../../../service/caching_object_store_service.dart';
+import '../../../service/entity_events_service.dart';
+import '../../../service/settings_service.dart';
+import '../../../service/status_service.dart';
+import '../../../service/user_auth_service.dart';
+import '../../../service/user_context_service.dart';
+import '../../../util/page_title.dart' as page_title;
+import '../../common/dime_directives.dart';
+import '../../select/select.dart';
+import '../entity_overview.dart';
 
 /// In this view, projects can be connected with certain keys. (There's currently only one that is relevant)
 ///   "Ferien": The project assigned with this key will be the one that is accounted for vacation time in [PeriodOverviewComponent]
