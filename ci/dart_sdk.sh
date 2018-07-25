9361469754dc635306394f191f97dc43eb40ae19
Andy Pfister <andy.pfister@hotmail.ch>
2018-07-25 11:19:20 +0200

    fixed dart sdk download link

--- ci/dart_sdk.sh
+++ ci/dart_sdk.sh
@@ -2,6 +2,6 @@
 
 if [ ! -f dart-sdk/bin/pub ]; then
   #todo replace with stable release once ready wget -q https://storage.googleapis.com/dart-archive/channels/stable/release/1.24.2/sdk/dartsdk-linux-x64-release.zip && \
-  wget -q https://storage.googleapis.com/dart-archive/channels/dev/release/2.0.0-dev.69.2/sdk/dartsdk-macos-x64-release.zip && \
+  wget -q https://storage.googleapis.com/dart-archive/channels/dev/release/2.0.0-dev.69.2/sdk/dartsdk-linux-x64-release.zip && \
   unzip -q dartsdk-linux-x64-release.zip
 fi
