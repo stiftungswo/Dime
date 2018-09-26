6940e75dbd78a4e73f9925d32048aea1e73738d9
Andy Pfister <andy.pfister@hotmail.ch>
2018-09-26 11:15:53 +0200

    added editing of a person

---
+++ src/Dime/FrontendBundle/Resources/public/lib/src/component/edit/person_edit_component.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:angular/angular.dart';
+import 'package:angular_forms/angular_forms.dart';
+import 'package:angular_router/angular_router.dart';
+
+import '../../model/entity_export.dart';
+import '../../service/caching_object_store_service.dart';
+import '../../service/entity_events_service.dart';
+import '../../service/status_service.dart';
+import '../../service/user_auth_service.dart';
+import '../../util/page_title.dart' as page_title;
+import '../common/dime_directives.dart';
+import 'entity_edit.dart';
+
+@Component(
+    selector: 'person-edit', templateUrl: 'person_edit_component.html', directives: const [coreDirectives, formDirectives, dimeDirectives])
+class PersonEditComponent extends EntityEdit<Person> {
+  PersonEditComponent(
+      CachingObjectStoreService store, StatusService status, UserAuthService auth, Router router, EntityEventsService entityEventsService)
+      : super(store, Person, status, auth, router, entityEventsService);
+
+  @override
+  void onActivate(RouterState _, RouterState current) {
+    super.onActivate(_, current);
+    reload();
+  }
+
+  @override
+  Future reload({bool evict: false}) async {
+    await super.reload(evict: evict);
+    page_title.setPageTitle('Person', entity?.lastName);
+  }
+}
