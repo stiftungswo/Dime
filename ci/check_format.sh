37fae9f61f65e72e500c336b8d36754b9abb0b42
Andy Pfister <andy.pfister@hotmail.ch>
2018-10-15 15:19:36 +0200

    added travis jobs for api

---
+++ ci/check_format.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -vu
+
+# checks if formatters could have been run locally to fix the code style
+
+# check backend (api)
+composer global require "squizlabs/php_codesniffer=*"
+# we run phpcbf twice to check if it was able to fix something
+# since it reports more than it can fix we check the status code twice and check if it changed
+# sadly there is no unique status code for "phpcbf fixed something and maybe there are unfixable errors"
+$HOME/.composer/vendor/bin/phpcbf --standard=psr2 --ignore=vendor api/
+code1=$?
+$HOME/.composer/vendor/bin/phpcbf --standard=psr2 --ignore=vendor api/ > /dev/null 2>&1
+code2=$?
+if [ $code1 -ne $code2 ]; then
+  echo "PHP Api is not properly formatted. Please reformat and commit again."
+  exit 1;
+fi
